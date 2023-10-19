7e12378ff3ba99480773c87235d6c9334af1763f
sushma subramaniyan <sushmasubramaniyan14@gmail.com>
2023-10-19 09:00:21 +0200

    working

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const logger = require("morgan");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -10,7 +11,14 @@ app.use(express.json());
 
 
 // Iteration 1 - Connect to MongoDB
+
 // DATABASE CONNECTION
+const MONGODB_URI = "mongodb://127.0.0.1:27017/express-mongoose-recipes-dev";
+
+mongoose
+  .connect(MONGODB_URI)
+  .then((x) => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+  .catch((err) => console.error("Error connecting to mongo", err));
 
 
 
@@ -23,11 +31,52 @@ app.get('/', (req, res) => {
 
 //  Iteration 3 - Create a Recipe route
 //  POST  /recipes route
+app.post('/recipes', async(req, res) => {
+    Recipe.create({
+        title: req.body.title,
+      instructions: req.body.instructions,
+      level: req.body.level,
+      ingredients: req.body.ingredients,
+      image: req.body.image,
+      duration: req.body.duration,
+      isArchived: req.body.isArchived,
+      created: req.body.created,
+    })
+
+    .then((createdRecipe)=>{
+        response.status(201).json(createdRecipe);
+    })
+        
+     .catch ((error)=> {
+        res.status(500).json({message: "not created recipe"})
+        
+    })
+});
+/*app.post('/recipes', async (request, response) => {
+    try {
+      const createRecipe = await Recipe.create(request.body)
+      response.status(201).json({ recipe: createRecipe })
+    } catch (error) {
+      console.log(error)
+      response.status(400).json({ error })
+    }
+  })*/
+
 
 
 //  Iteration 4 - Get All Recipes
 //  GET  /recipes route
-
+app.get('/recipes',async(req,res)=>{
+   Recipe.find()
+    .then((allRecipe)=>{
+        response.status(201).json(allRecipe);
+    })
+        
+     .catch ((error)=> {
+        res.status(500).json({message: "not created recipe"})
+        
+    })
+});
 
 //  Iteration 5 - Get a Single Recipe
 //  GET  /recipes/:id route
